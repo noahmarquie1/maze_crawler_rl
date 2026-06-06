4d153fa3d293de9203373559d55c49b5cf957129
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-06-06 16:13:33 -0700

    extract evaluate_model to function

--- evaluate_model.py
+++ evaluate_model.py
@@ -4,16 +4,24 @@ from stable_baselines3 import PPO
 from agent import CrawlEnv
 from kaggle_environments import make
 
-crawl_env = CrawlEnv()
 
-model = PPO.load(MODEL_PATH)
-obs, _ = crawl_env.reset()
+def run_episode(env: CrawlEnv, model: PPO):
+    obs, _ = crawl_env.reset()
 
-done = False
-while not done:
-    action, _ = model.predict(obs)
-    obs, reward, done, truncated, info = crawl_env.step(action.item())
+    done = False
+    while not done:
+        action, _ = model.predict(obs)
+        obs, reward, done, truncated, info = crawl_env.step(action.item())
 
-html_out = crawl_env.render(mode="html", width=800, height=800)
-with open("replay.html", "w") as f:
-    f.write(html_out)
+
+if __name__ == "__main__":
+    crawl_env = CrawlEnv()
+
+    model = PPO.load(MODEL_PATH)
+    run_episode(crawl_env, model)
+    html_out = crawl_env.render(mode="html", width=800, height=800)
+    if html_out is not None:
+        with open("replay.html", "w") as f:
+            f.write(html_out)
+
+    print(f"Evaluation Finished successfully.")

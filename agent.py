7a5b70ba708f30a85067603909d5aee2611946b5
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-06-06 17:59:31 -0700

    save model on error

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ from kaggle_environments import make
 from kaggle_environments.utils import Struct
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.monitor import Monitor
 import os
 
 from model import CNNFeatureExtractor
@@ -94,25 +95,24 @@ class CrawlEnv(gym.Env):
 if __name__ == "__main__":
     out = "ppo_crawl"
     n_envs = 8
-    env = SubprocVecEnv([lambda: CrawlEnv() for _ in range(n_envs)])
+    env = SubprocVecEnv([lambda: Monitor(CrawlEnv()) for _ in range(n_envs)])
+    agent = PPO(
+        "MlpPolicy",
+        env,
+        n_steps=512,
+        policy_kwargs={"features_extractor_class": CNNFeatureExtractor},
+        batch_size=128,
+        verbose=1,
+    )
     try:
-        agent = PPO(
-            "MlpPolicy",
-            env,
-            n_steps=512,
-            policy_kwargs={"features_extractor_class": CNNFeatureExtractor},
-            batch_size=128,
-            verbose=1,
-        )
         agent.learn(total_timesteps=int(1e5), log_interval=1, progress_bar=True)
-        if os.path.exists(out + ".zip"):
-            os.remove(out + ".zip")
-
-        agent.save(out)
 
     except Exception as e:
         raise e
     finally:
+        if os.path.exists(out + ".zip"):
+            os.remove(out + ".zip")
+        agent.save(out)
         html_out = env.render(mode="html")
         if html_out is not None:
             with open(os.path.join("error_replay.html"), "w") as f:

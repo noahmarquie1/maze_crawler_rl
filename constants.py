ea266971b72f52f94bac1667d01c69408737b310
Noah <noah.marquie@gmail.com>
2026-06-06 17:04:32 -0700

    Changed constants

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ DEBUG = False
 MODEL_PATH = "ppo_crawl"
 REPLAY_OUTPUT_DIR = "output_replays"
 ACTION_MAPPING = {
-    0: "JUMP_NORTH",
-    1: "JUMP_SOUTH",
-    2: "JUMP_EAST",
-    3: "JUMP_WEST",
+    0: "NORTH",
+    1: "SOUTH",
+    2: "EAST",
+    3: "WEST",
 }

7110e2f4b087df04527083ac27d99dfb972eef86
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-06-08 15:41:18 -0700

    incrementally save checkpoints

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import CheckpointCallback
 import os
 from env import CrawlEnv
 
@@ -9,18 +10,33 @@ from model import CNNFeatureExtractor
 
 if __name__ == "__main__":
     out = "ppo_crawl"
+    checkpoint_dir = "checkpoints"
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
     n_envs = 32
     env = SubprocVecEnv([lambda: Monitor(CrawlEnv()) for _ in range(n_envs)])
     agent = PPO(
         "MlpPolicy",
         env,
         n_steps=512,
-        #policy_kwargs={"features_extractor_class": CNNFeatureExtractor},
+        policy_kwargs={"features_extractor_class": CNNFeatureExtractor},
         batch_size=128,
         verbose=1,
     )
+
+    checkpoint_callback = CheckpointCallback(
+        save_freq=50_000 // n_envs,
+        save_path=checkpoint_dir,
+        name_prefix=out,
+    )
+
     try:
-        agent.learn(total_timesteps=int(4e5), log_interval=1, progress_bar=True)
+        agent.learn(
+            total_timesteps=int(4e5),
+            log_interval=1,
+            progress_bar=True,
+            callback=checkpoint_callback,
+        )
 
     except Exception as e:
         raise e
